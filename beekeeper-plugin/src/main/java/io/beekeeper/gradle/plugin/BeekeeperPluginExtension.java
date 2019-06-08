cac65fb1843356278c4febe58cc09fa001b9561c
Filip Wieladek <filip@beekeeper.io>
2019-06-08 12:08:28 +0200

    Fix formatting

--- beekeeper-plugin/src/main/java/io/beekeeper/gradle/plugin/BeekeeperPluginExtension.java
+++ beekeeper-plugin/src/main/java/io/beekeeper/gradle/plugin/BeekeeperPluginExtension.java
@@ -5,7 +5,8 @@ import lombok.Data;
 @Data
 public class BeekeeperPluginExtension {
 
-    private static final String DEFAULT_REQUIRED_VERSION_URL = "https://raw.githubusercontent.com/beekpr/beekeeper-gradle-plugins/master/REQUIRED_VERSION";
+    private static final String DEFAULT_REQUIRED_VERSION_URL =
+        "https://raw.githubusercontent.com/beekpr/beekeeper-gradle-plugins/master/REQUIRED_VERSION";
 
     private String requiredVersionUrl = DEFAULT_REQUIRED_VERSION_URL;
 }
