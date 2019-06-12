da9470de4286a3480826cc2d96eaecd82f224da6
Filip Wieladek <filip@beekeeper.io>
2019-06-12 22:20:10 +0200

    Enable all coverage reporting

--- beekeeper-testing-plugin/src/main/java/io/beekeeper/gradle/testing/TestingPlugin.java
+++ beekeeper-testing-plugin/src/main/java/io/beekeeper/gradle/testing/TestingPlugin.java
@@ -3,6 +3,7 @@ package io.beekeeper.gradle.testing;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension;
+import org.gradle.testing.jacoco.tasks.JacocoReport;
 
 public class TestingPlugin implements Plugin<Project> {
 
@@ -10,10 +11,23 @@ public class TestingPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
+        applyJacoco(project);
+    }
+
+    private void applyJacoco(Project project) {
         project.getPluginManager().apply("jacoco");
         JacocoPluginExtension jacoco = project.getExtensions().getByType(JacocoPluginExtension.class);
-
         jacoco.setToolVersion("0.8.4");
+
+        project.afterEvaluate(this::configureJacoco);
+    }
+
+    private void configureJacoco(Project project) {
+        project.getTasks().withType(JacocoReport.class, task -> {
+            task.getReports().getXml().setEnabled(true);
+            task.getReports().getCsv().setEnabled(true);
+            task.getReports().getHtml().setEnabled(true);
+        });
     }
 
 }
