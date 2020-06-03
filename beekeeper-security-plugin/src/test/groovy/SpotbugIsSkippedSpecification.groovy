263ef9b2f6974525e1778c90dc4c93f52eceb439
pawelebe <pawel.englert@beekeeper.io>
2020-06-03 08:10:53 +0200

    FUL-20839: skip spotbugs from owasp check

---
+++ beekeeper-security-plugin/src/test/groovy/SpotbugIsSkippedSpecification.groovy
@@ -0,0 +1,77 @@
+import io.beekeeper.gradle.security.SecurityPlugin
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class SpotbugIsSkippedSpecification extends Specification {
+
+    @Rule
+    TemporaryFolder dir
+    GradleRunner runner
+
+    def setup() {
+        runner = GradleRunner.create()
+            .withProjectDir(dir.root)
+            .withPluginClasspath()
+    }
+
+    def "it should not report spotbug vulnerability"() {
+        given:
+        setUpBuildGradle(true)
+
+        when:
+        BuildResult result = runner.withArguments('dependencyCheckAnalyze').build()
+
+        then:
+        result.output.contains("Found 0 vulnerabilities")
+    }
+
+    def "it should report spotbug vulnerability with no supression"() {
+        //NOTE: this test may start failing once spotbugs has no vulnerabilities
+        given:
+        setUpBuildGradle(false)
+
+        when:
+        BuildResult result = runner.withArguments('dependencyCheckAnalyze').build()
+
+        then:
+        !result.output.contains("Found 0 vulnerabilities")
+    }
+
+    void setUpBuildGradle(boolean useCommonSuppression){
+        buildFile << """
+
+
+        plugins {
+            id '${SecurityPlugin.IDENTIFIER}'
+            id "com.github.spotbugs" version "2.0.0"
+
+        }
+        apply plugin: 'java'
+
+          repositories {
+            mavenCentral()
+        }
+
+        beekeeperSecurityExtension{
+            applyCommonSuppressions ${useCommonSuppression}
+        }
+
+        """
+    }
+
+    File getBuildFile() {
+        file('build.gradle')
+    }
+
+    File file(String path) {
+        File f = new File(dir.root, path)
+        if (!f.exists()) {
+            f.parentFile.mkdirs()
+            return dir.newFile(path)
+        }
+        return f
+    }
+}
