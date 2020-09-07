684fc306e046f298bcb6abce3ca21e89bf8f776a
wscsprint3r <stefan.irimescu@beekeeper.io>
2020-09-07 13:34:14 +0200

    FUL-22325-5: disable spotbugs for quarkus

--- beekeeper-code-analysis-plugin/src/main/java/io/beekeeper/gradle/code/CodeAnalysisPlugin.java
+++ beekeeper-code-analysis-plugin/src/main/java/io/beekeeper/gradle/code/CodeAnalysisPlugin.java
@@ -1,11 +1,13 @@
 package io.beekeeper.gradle.code;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
 
@@ -18,6 +20,7 @@ public class CodeAnalysisPlugin implements Plugin<Project> {
     public static final String IDENTIFIER = "io.beekeeper.gradle.plugins.code-analysis-check";
     private static final String SPOT_BUGS_VERSION = "4.0.0";
     private static final String FIND_SEC_BUGS_VERSION = "1.10.1";
+    private static final String QUARKUS_DEPENDENCY_GROUP = "io.quarkus";
 
     @Override
     public void apply(Project project) {
@@ -47,13 +50,26 @@ public class CodeAnalysisPlugin implements Plugin<Project> {
 
     private void configureTasks(Project project) {
         project.getTasks().withType(SpotBugsTask.class, task -> {
-            boolean isJenkins = isJenkins();
-            task.getReports().getXml().setEnabled(isJenkins);
-            task.getReports().getHtml().setEnabled(!isJenkins);
+            if(isQuarkusProject(project)) {
+                task.setEnabled(false);
+            } else {
+                boolean isJenkins = isJenkins();
+                task.getReports().getXml().setEnabled(isJenkins);
+                task.getReports().getHtml().setEnabled(!isJenkins);
+            }
         });
     }
 
     private boolean isJenkins() {
         return System.getenv("JENKINS_URL") != null;
+    }
+
+    private boolean isQuarkusProject(Project project) {
+        return project
+                .getConfigurations()
+                .stream()
+                .map(Configuration::getAllDependencies)
+                .flatMap(Collection::stream)
+                .anyMatch(d -> QUARKUS_DEPENDENCY_GROUP.equals(d.getGroup()));
     }
 }
