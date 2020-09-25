61b2e19b90062dba1b32c6acfe2c1cf81aaafc37
Filip Wieladek <filip@beekeeper.io>
2020-09-25 18:49:30 +0200

    Fix compile issues from version upgrade

--- beekeeper-code-analysis-plugin/src/main/java/io/beekeeper/gradle/code/CodeAnalysisPlugin.java
+++ beekeeper-code-analysis-plugin/src/main/java/io/beekeeper/gradle/code/CodeAnalysisPlugin.java
@@ -1,24 +1,21 @@
 package io.beekeeper.gradle.code;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.plugins.JavaPluginConvention;
-import org.gradle.api.tasks.SourceSet;
 
-import com.github.spotbugs.SpotBugsExtension;
-import com.github.spotbugs.SpotBugsPlugin;
-import com.github.spotbugs.SpotBugsTask;
+import com.github.spotbugs.snom.SpotBugsExtension;
+import com.github.spotbugs.snom.SpotBugsPlugin;
+import com.github.spotbugs.snom.SpotBugsTask;
 
 public class CodeAnalysisPlugin implements Plugin<Project> {
 
     public static final String IDENTIFIER = "io.beekeeper.gradle.plugins.code-analysis-check";
-    private static final String SPOT_BUGS_VERSION = "4.0.0";
+    private static final String SPOT_BUGS_VERSION = "4.1.3";
     private static final String FIND_SEC_BUGS_VERSION = "1.10.1";
     private static final String QUARKUS_DEPENDENCY_GROUP = "io.quarkus";
 
@@ -28,21 +25,19 @@ public class CodeAnalysisPlugin implements Plugin<Project> {
             project.getPluginManager().apply(SpotBugsPlugin.class);
 
             SpotBugsExtension config = project.getExtensions().getByType(SpotBugsExtension.class);
-            config.setToolVersion(SPOT_BUGS_VERSION);
-
-            SourceSet main = project.getConvention()
-                .getPlugin(JavaPluginConvention.class)
-                .getSourceSets()
-                .getByName("main");
+            config.getToolVersion().set(SPOT_BUGS_VERSION);
 
             Map<String, String> findSecBugsDependency = new HashMap<>();
             findSecBugsDependency.put("group", "com.h3xstream.findsecbugs");
             findSecBugsDependency.put("name", "findsecbugs-plugin");
             findSecBugsDependency.put("version", FIND_SEC_BUGS_VERSION);
-
             project.getDependencies().add("spotbugsPlugins", findSecBugsDependency);
 
-            config.setSourceSets(Collections.singleton(main));
+            Map<String, String> spotbugsAnnotation = new HashMap<>();
+            spotbugsAnnotation.put("group", "com.github.spotbugs");
+            spotbugsAnnotation.put("name", "spotbugs-annotations");
+            spotbugsAnnotation.put("version", config.getToolVersion().get());
+            project.getDependencies().add("compileOnly", spotbugsAnnotation);
         });
 
         project.afterEvaluate(this::configureTasks);
@@ -54,8 +49,8 @@ public class CodeAnalysisPlugin implements Plugin<Project> {
                 task.setEnabled(false);
             } else {
                 boolean isJenkins = isJenkins();
-                task.getReports().getXml().setEnabled(isJenkins);
-                task.getReports().getHtml().setEnabled(!isJenkins);
+                task.getReports().getByName("xml").setEnabled(isJenkins);
+                task.getReports().getByName("html").setEnabled(isJenkins);
             }
         });
     }
