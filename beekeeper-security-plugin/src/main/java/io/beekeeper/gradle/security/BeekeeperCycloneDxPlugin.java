207a37fb3aeebd40313e3c8b764bcdadb4df5b34
Andrei Isac <andrei.isac@beekeeper.io>
2022-01-10 22:03:09 +0200

    FUL-32474: force cyclonedxBom task to run sequencially

--- beekeeper-security-plugin/src/main/java/io/beekeeper/gradle/security/BeekeeperCycloneDxPlugin.java
+++ beekeeper-security-plugin/src/main/java/io/beekeeper/gradle/security/BeekeeperCycloneDxPlugin.java
@@ -3,6 +3,9 @@ package io.beekeeper.gradle.security;
 import org.cyclonedx.gradle.CycloneDxPlugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.services.BuildService;
+import org.gradle.api.services.BuildServiceParameters;
 
 public class BeekeeperCycloneDxPlugin implements Plugin<Project> {
 
@@ -11,6 +14,25 @@ public class BeekeeperCycloneDxPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         project.getPluginManager().apply(CycloneDxPlugin.class);
+
+        Provider<LockService> lockService = project.getGradle()
+            .getSharedServices()
+            .registerIfAbsent(
+                "lockService",
+                LockService.class,
+                (spec) -> {
+                    spec.getMaxParallelUsages().set(1);
+                }
+            );
+
+        project.getTasks().getByPath("cyclonedxBom").usesService(lockService);
+    }
+
+    public static class LockService implements BuildService<BuildServiceParameters.None> {
+        @Override
+        public BuildServiceParameters.None getParameters() {
+            return null;
+        }
     }
 
 }
