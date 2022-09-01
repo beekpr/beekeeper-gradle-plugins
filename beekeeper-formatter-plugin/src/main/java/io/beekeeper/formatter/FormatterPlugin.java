ba442bfc22ffbab0c4cc59aa2b4a2b4da0830b6f
Michał Koźmiński <michal@kozminski.co>
2022-09-01 14:37:26 +0200

    MK-7 Use google code formatting as default formatter

--- beekeeper-formatter-plugin/src/main/java/io/beekeeper/formatter/FormatterPlugin.java
+++ beekeeper-formatter-plugin/src/main/java/io/beekeeper/formatter/FormatterPlugin.java
@@ -19,9 +19,6 @@ public class FormatterPlugin implements Plugin<Project> {
     private static final String PLUGIN_GROOVY = "groovy";
     private static final GradleVersion MIN_GRADLE_VERSION_SUPPORTED = GradleVersion.version("4.6");
 
-    private static final String JAVA_FORMATTING_RULES_RELATIVE_PATH =
-        "beekeeper-formatter-rules/java/BeekeeperCodeFormat.xml";
-
     @Override
     public void apply(Project project) {
         gradleVersionCheck(project);
@@ -54,10 +51,9 @@ public class FormatterPlugin implements Plugin<Project> {
         // our plugin before the java plugin
         project.getPluginManager().withPlugin(PLUGIN_JAVA, javaPlugin -> {
             extension.java(java -> {
+                java.googleJavaFormat();
                 java.removeUnusedImports();
                 java.trimTrailingWhitespace();
-
-                java.eclipse().configFile(getJavaRulesPath(project));
             });
         });
     }
@@ -92,19 +88,9 @@ public class FormatterPlugin implements Plugin<Project> {
     }
 
     private void createFormatTask(Project project) {
-        project.getTasks().create("extractBeekeeperFormattingConfig", ExtractResourceTask.class, (task) -> {
-            task.setResourcePath(JAVA_FORMATTING_RULES_RELATIVE_PATH);
-            task.setDestination(getJavaRulesPath(project));
-        });
-
         // Make the spotless tasks dependent on the config extraction task,
         // so they can reach the config files
         project.getTasksByName("spotlessCheck", true).forEach(t -> t.dependsOn("extractBeekeeperFormattingConfig"));
         project.getTasksByName("spotlessApply", true).forEach(t -> t.dependsOn("extractBeekeeperFormattingConfig"));
-    }
-
-    @SuppressFBWarnings("PATH_TRAVERSAL_IN")
-    private String getJavaRulesPath(Project project) {
-        return Paths.get(project.getBuildDir().getAbsolutePath(), JAVA_FORMATTING_RULES_RELATIVE_PATH).toString();
     }
 }
